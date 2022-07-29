0bf3336fb909f0e0566f34625cef9ef53b075d17
roflmaostc <roflmaostc@users.noreply.github.com>
2022-07-29 14:34:22 +0000

    jekyll build from Action a6f1c9f4a29cc3657f205681e55d2d17362b32e2

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "DeconvOptim.jl",
+        "excerpt":"Recently, we worked on a deconvolution toolbox written in Julia Lang, called DeconvOptim.jl. It can deconvolve multi color data very efficiently with a quality equal to Huygens Deconvolution. The routine is very generic and based on a loss function approach which is minimized with the automatic differentiation Zygote.jl and the...","categories": ["post","archive"],
+        "tags": [],
+        "url": "/post/archive/2022/02/17/DeconvOptim.html",
+        "teaser": "/assets/posts/color-deconv.jpg"
+      }]
