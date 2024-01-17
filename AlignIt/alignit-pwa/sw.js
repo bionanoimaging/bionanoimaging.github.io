868c211150724cc4282be8dbe774ab2032be05f0
RainerHeintzmann <heintzmann@gmail.com>
2024-01-17 11:24:01 +0100

    update of AlignIt with PWA support

---
+++ AlignIt/alignit-pwa/sw.js
@@ -0,0 +1,21 @@
+// based on the instructions at https://github.com/codepo8/github-page-pwa
+// Change this to your repository name
+var GHPATH = '/alignit-pwa';
+
+// Choose a different app prefix name
+var APP_PREFIX = 'alignitpwa_';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/css/styles.css`,
+  `${GHPATH}/js/alignit.js`
+]
