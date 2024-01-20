fed12930d7701fef63b1c87fca250a6bdf566dec
RainerHeintzmann <heintzmann@gmail.com>
2024-01-20 21:13:34 +0100

    AlignIt update

--- AlignIt/sw.js
+++ AlignIt/sw.js
@@ -27,6 +27,7 @@ const cacheName = APP_PREFIX+VERSION;
 const filesToCache = [
   GHPATH+'/',
   GHPATH+'/index.html',
+  GHPATH+'/manifest.webmanifest',
   GHPATH+'/css/style.css',
   GHPATH+'/js/alignit.js',
   GHPATH+'/js/components.js',
@@ -89,35 +90,66 @@ const filesToCache = [
   WPATH+'/lvl_12.json'
 ];
 
-// Install event - cache the files during service worker installation
+
+// From https://googlechrome.github.io/samples/service-worker/basic/
+
+// const PRECACHE_URLS = [
+//   "index.html", "js/babylon.js", "js/babylon.glTFFileLoader.js", "js/babylon.gui.min.js", "js/pep.min.js", "js/applecrushervr.js",
+//   "textures/environment.dds", "textures/flare.png", "images/LogoApplesCrusher300transparent.png", "css/main.css",
+//   "assets/ApplesCrusherBackground.glb.manifest", "assets/Apple.glb.manifest", "assets/AppleCrushed.wav", "assets/RunningForGlory.mp3",
+//   "manifest.json", "assets/186950__readeonly__toy-cannon-shot.wav", "assets/bananapistolwithammo.glb.manifest", "assets/lasersaber.glb.manifest"
+// ];
+
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(filesToCache);
-    })
+      caches.open(PRECACHE)
+          .then(cache => cache.addAll(filesToCache))
+          .then(self.skipWaiting())
   );
 });
 
-// Fetch event - intercept network requests and serve from cache if available
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Activate event - clean up old caches
+// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(existingCacheName => {
-          if (existingCacheName !== cacheName) {
-            return caches.delete(existingCacheName);
-          }
-        })
-      );
-    })
+      caches.keys().then(cacheNames => {
+          return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+      }).then(cachesToDelete => {
+          return Promise.all(cachesToDelete.map(cacheToDelete => {
+              return caches.delete(cacheToDelete);
+          }));
+      }).then(() => self.clients.claim())
   );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(
+          caches.match(event.request, {ignoreSearch: true}).then(cachedResponse => {
+              if (cachedResponse) {
+                  return cachedResponse;
+              }
+
+              return caches.open(RUNTIME).then(cache => {
+                  return fetch(event.request).then(response => {
+                      // Put a copy of the response in the runtime cache.
+                      return cache.put(event.request, response.clone()).then(() => {
+                          return response;
+                      });
+                  });
+              });
+          })
+      );
+  }
 });
