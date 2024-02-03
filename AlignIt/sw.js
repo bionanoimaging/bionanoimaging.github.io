e273bcba5edbfa5bd3d4ebb0b09fc97a5d832bda
RainerHeintzmann <heintzmann@gmail.com>
2024-02-03 11:16:40 +0100

    AlignIt update

--- AlignIt/sw.js
+++ AlignIt/sw.js
@@ -5,7 +5,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v014';
+const PRECACHE = 'precache-v015';
 const RUNTIME = 'runtime';
 
 // The files to make available for offline use. make sure to add 
