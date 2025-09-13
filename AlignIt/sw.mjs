12086f24ca368c67105e3ec2956e195f98c61764
RainerHeintzmann <heintzmann@gmail.com>
2025-09-13 20:47:31 +0200

    bug fixes and speed improvement

--- AlignIt/sw.mjs
+++ AlignIt/sw.mjs
@@ -1,7 +1,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v20';
+const PRECACHE = 'precache-v22';
 const RUNTIME = 'runtime';
 
 // The files we want to cache for offline use. These are all relative to the
